4d28a19c7d24a18a5fa9d16cdc9469080f1cac68
kartik8095 <kartikkashid809@gmail.com>
2026-07-06 12:09:52 +0530

    Completed authentication and dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Dashboard() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const res = await api.get("/auth/profile", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        setUser(res.data.user);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  );
+  <div
+    style={{
+      padding: "40px",
+      background: "#f5f7fb",
+      minHeight: "100vh",
+      color: "#222",
+    }}
+  >
+    <h1>🎉 Dashboard</h1>
+
+    {user && (
+      <>
+        <h2>Welcome {user.email}</h2>
+        <p>User ID: {user.id}</p>
+      </>
+    )}
+  </div>
+);
 }
 
 export default Dashboard;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../services/api";
+import "../styles/Login.css";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await api.post("/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login Successful");
+
+      navigate("/dashboard");
+    } catch (err: any) {
+  console.log("ERROR:", err);
+  console.log("RESPONSE:", err.response);
+
+  alert(err.response?.data?.message || err.message);
+}
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="login-container">
+      <div className="login-card">
+        <h1>SkillBridge AI</h1>
+        <h2>Welcome Back 👋</h2>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={handleLogin}>Login</button>
+
+        <p>
+          Don't have an account?
+          <Link to="/register"> Register</Link>
+        </p>
+      </div>
     </div>
   );
 }
